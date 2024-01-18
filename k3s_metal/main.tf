e3d44895e68d407cc722a140aa3dd827a4c06746
abasitt <abdul.basit@rakuten.com>
2024-01-18 13:55:37 +0000

    adding metal cluster to tf

---
+++ k3s_metal/main.tf
@@ -0,0 +1,63 @@
+locals {
+  common = {
+    memory = 16384
+    vcpu = 4
+    disk_size = 250
+    bridgename = "br30"
+    iso_path = "/home/abasit/downloads/cloud-images/ubuntu-22-cloud-image/ubuntu22-disk.qcow2"
+    ipv4mask = "24"
+    ipv4gw   = "192.168.30.1"
+    ipv4ns   = "192.168.30.1"
+    ipv6mask = "64"
+    ipv6gw   = "2001:470:ee86:30::1"
+    ipv6ns   = "2001:470:ee86:30::1"
+  }
+  host1 = {
+    pool1 = "zp_sda_sdb-images"
+    pool2 = "zp_sda_sdb-images-critical" # pool with zfs level sync enabled between host1 and host2
+    pool3 = "zp_nvmpci1-images" # pool on nvme disk
+    pool4 = "zp_nvmpci1-images-critical"
+    pool5 = "default"
+  }
+  host2 = {
+    pool1 = "zp_hdd01-images"
+    pool2 = "zp_hdd01-images-critical"
+    pool3 = "zp_ssd01-images"
+    pool4 = "zp_ssd01-images-critical"
+    pool5 = "default"
+  }
+}
+
+
+module "k8s_vm1" {
+  source = "../modules/libvirt_vm"
+  
+  # Set input variables here
+  vm_hostname_prefix = "k3s_metalm1"
+  autostart          = true
+  memory             = local.common.memory
+  vcpu               = local.common.vcpu
+  dhcp               = false
+  ip_address         = ["192.168.30.41"]
+  ip6_address        = ["2001:470:ee86:30:192:168:30:41"]
+  ip_gateway         = local.common.ipv4gw
+  ip6_gateway        = local.common.ipv6gw
+  ip_nameserver      = local.common.ipv4ns
+  ip6_nameserver     = local.common.ipv6ns
+  bridge             = local.common.bridgename
+  pool               = local.host1.pool4
+  system_volume      = local.common.disk_size
+  ssh_admin          = "k8s"
+  ssh_admin_passwd   = "$6$rounds=4096$hSjO/nCBqa/ottYL$mg7Z4dlx6FR0Tpy1NOn.cWJ9926sfr0bV9V/gVwNUIyKHU9nHsYhqpbtaQjLEjuANW0BMRUiTiJe7PjAV4eER1"
+  root_passwd        = "$6$rounds=4096$hSjO/nCBqa/ottYL$mg7Z4dlx6FR0Tpy1NOn.cWJ9926sfr0bV9V/gVwNUIyKHU9nHsYhqpbtaQjLEjuANW0BMRUiTiJe7PjAV4eER1"
+  ssh_private_key    = "~/.ssh/terraform_vm"
+  ssh_keys    = [
+    chomp(file("~/.ssh/terraform_vm.pub"))
+    ]
+  os_img_url         = "file:///home/abasit/downloads/iso/jammy-server-cloudimg-amd64.img"
+
+  # Required provider configuration
+  providers = {
+    libvirt = libvirt.host1
+  }
+}
---
+++ k3s_metal/provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "abasit"
+
+    workspaces {
+      name = "home-libvirt"
+    }
+  }
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+# Define providers
+provider "libvirt" {
+  alias = "host1"
+  uri   = "qemu+ssh://abasit@192.168.10.5/system?keyfile=/home/abasit/.ssh/id_ed25519&no_verify=1"
+}
+
+provider "libvirt" {
+  alias = "host2"
+  uri   = "qemu+ssh://abasit@192.168.10.7/system?keyfile=/home/abasit/.ssh/id_ed25519&no_verify=1"
+}
+
+provider "libvirt" {
+  alias = "host3"
+  uri   = "qemu+ssh://abasit@192.168.10.8/system?keyfile=/home/abasit/.ssh/id_ed25519&no_verify=1"
+}
